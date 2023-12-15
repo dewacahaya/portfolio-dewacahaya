40474bc20f151c4751c71b2dd4968bef6dac5a89
dewacahaya <dewacahaya30@gmail.com>
2023-12-15 12:45:29 +0800

    minify css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ["./public/**/*.{html,js}", "./.{html,js}"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       fontFamily: {
-        poppins: ['Poppins'],
-        quicksand: ['Quicksand']
+        poppins: ["Poppins"],
+        quicksand: ["Quicksand"],
       },
       colors: {
-        primary: ['#053B50'],
-        navbar: ['#176B87'],
-        hover: ['#64CCC5'],
-        text: ['#EEEEEE'],
+        primary: ["#053B50"],
+        navbar: ["#176B87"],
+        hover: ["#64CCC5"],
+        text: ["#EEEEEE"],
       },
       screens: {
-        '2xl': '1328px'
-      }
+        "2xl": "1328px",
+      },
     },
   },
   plugins: [],
-}
-
+};
