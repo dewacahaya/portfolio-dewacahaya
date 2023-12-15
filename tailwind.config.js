f8ce84ec6f1561d2db7be2f9558549bd3504b81d
dewacahaya <dewacahaya30@gmail.com>
2023-12-15 13:10:59 +0800

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}", "./**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}", "./script/*.js"],
   theme: {
     container: {
       center: true,
